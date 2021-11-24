aaa7d46b9a9e9d14dbe5601b836d83abae87b4a9
Oscar Cornejo <oscarcornejo10@gmail.com>
2021-11-24 01:20:42 -0300

    add style button css

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+// NEW
+import postcss from "rollup-plugin-postcss";
+
 const packageJson = require("./package.json");
 
 export default [
@@ -20,11 +23,21 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+
+      // NEW
+      postcss(),
+    ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+
+    // NEW
+    external: [/\.css$/],
   },
 ];
